50eaecb9554a7937a28c5fe819387129109a309f
Jnmendza <jonatmendoza@gmail.com>
2019-09-17 14:31:39 -0700

    1st Stretch complete

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { NavLink } from "react-router-dom";
 
 const MovieCard = props => {
-  return;
+  return(
+    <NavLink key={props.id} to={`/movies/${props.id}`}>
+      <div className="movie-card">
+        <h2>{props.title}</h2>
+
+        <div className="movie-director">
+          Director: <em>{props.director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{props.metascore}</strong>
+        </div>
+        <h3>Actors</h3>
+        {props.stars ? props.stars.map(star => (
+          <div key={star} className="movie-star">
+            {star}
+          </div>
+        )): null}
+
+      </div>
+    </NavLink>
+  )
 };
 
 export default MovieCard;
